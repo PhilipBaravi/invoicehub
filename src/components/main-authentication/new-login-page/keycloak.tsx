e60b7c6cbe88f9fcc6dcba0875a49779a983d8ae
PhilipBaravi <f.baravi121@gmail.com>
2024-12-01 17:17:00 +0200

    Change keycloak auth url

--- src/components/main-authentication/new-login-page/keycloak.tsx
+++ src/components/main-authentication/new-login-page/keycloak.tsx
@@ -8,7 +8,7 @@ interface AuthTokens {
 }
 
 const keycloakConfig = {
-  url: 'http://localhost:8080/auth',
+  url: 'http://3.73.125.19:8080/auth',
   realm: 'e-invoices',
   clientId: 'invoicing-app-react-login',
   enableCors: true,
