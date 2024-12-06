04d48cfc80caabacc4cec5e141988fe84400f75f
PhilipBaravi <f.baravi121@gmail.com>
2024-12-06 17:56:34 +0200

    Again

--- src/landing-page/LandingAbout.tsx
+++ src/landing-page/LandingAbout.tsx
@@ -18,7 +18,7 @@ const LandingAbout: FC = () => {
         <Card>
         <div className="flex justify-between items-center">   
             <div className="w-full">
-            <img src="/invoicing-app/dashboardDark.png" alt="Dashboard UI" className="w-full rounded-2xl shadow-2xl" /> 
+            <img src={`${process.env.PUBLIC_URL}/images/dashboardDark.png"`} alt="Dashboard UI" className="w-full rounded-2xl shadow-2xl" /> 
             </div>
         </div>
         </Card>
