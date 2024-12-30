518a44377695cc60213413b185c20d3468256bba
Philip <f.baravi121@gmail.com>
2024-12-30 15:55:54 +0100

    Change env variables temporarily

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,11 +1,12 @@
 // src/lib/utils/constants.ts
 
 export const API_BASE_URL: string =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:9090/api/v1/";
+  import.meta.env.VITE_API_BASE_URL || "https://api.invoicehub.space/api/v1/";
 export const POST_LOGOUT_REDIRECT_URI: string =
-  import.meta.env.VITE_POST_LOGOUT_REDIRECT_URI || "http://localhost:5173/";
+  import.meta.env.VITE_POST_LOGOUT_REDIRECT_URI || "https://invoicehub.space/";
 export const KEYCLOAK_CONFIG_URL: string =
-  import.meta.env.VITE_KEYCLOAK_CONFIG_URL || "http://localhost:8080/auth";
+  import.meta.env.VITE_KEYCLOAK_CONFIG_URL ||
+  "https://api.invoicehub.space/auth";
 
 export const currencySymbols: { [key: string]: string } = {
   USD: "$",
